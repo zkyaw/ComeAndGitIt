4f6812c4f6d2d3dc71277cb35d3c11d5cdbcb4e2
Zildjian Quiaoit <202110424@feualabang.edu.ph>
2021-09-29 10:09:51 +0800

    Added subtraction feature.

--- calculator.cpp
+++ calculator.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    int firstNumber, secondNumber, sumOfTwoNumbers;
+    int firstNumber, secondNumber, sumOfTwoNumbers, diffOfTwoNumbers;
     
     cout << "Enter first number: ";
     cin >> firstNumber;
@@ -11,11 +11,12 @@ int main()
     cout << "Enter second number: ";
     cin >> secondNumber;
 
-    // sum of two numbers is stored in variable sumOfTwoNumbers
     sumOfTwoNumbers = firstNumber + secondNumber;
+    diffOfTwoNumbers = firstNumber - secondNumber;
 
     // Prints sum 
-    cout << endl << firstNumber << " + " <<  secondNumber << " = " << sumOfTwoNumbers;     
+    cout << endl << firstNumber << " + " << secondNumber << " = " << sumOfTwoNumbers;  
+	cout << endl << firstNumber << " - " << secondNumber << " = " << diffOfTwoNumbers;   
 
     return 0;
 }
